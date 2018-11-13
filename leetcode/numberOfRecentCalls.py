57af3cc1e3390aac80007dcd80bc0df2370df46a
Federico Martinez <federicoemartinez@gmail.com>
2018-11-12 22:04:59 -0300

    solution for number-of-recent-calls

---
+++ leetcode/numberOfRecentCalls.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/number-of-recent-calls/description/
+"""
+Write a class RecentCounter to count recent requests.
+
+It has only one method: ping(int t), where t represents some time in milliseconds.
+
+Return the number of pings that have been made from 3000 milliseconds ago until now.
+
+Any ping with time in [t - 3000, t] will count, including the current ping.
+
+It is guaranteed that every call to ping uses a strictly larger value of t than before.
+
+ 
+
+Example 1:
+
+Input: inputs = ["RecentCounter","ping","ping","ping","ping"], inputs = [[],[1],[100],[3001],[3002]]
+Output: [null,1,2,3,3]
+ 
+
+Note:
+
+Each test case will have at most 10000 calls to ping.
+Each test case will call ping with strictly increasing values of t.
+Each call to ping will have 1 <= t <= 10^9.
+
+"""
+from bisect import bisect_left
+
+class RecentCounter:
+
+    def __init__(self):
+        self.ping_time = []
+        self._min = 0
+
+    def ping(self, t):
+        self.ping_time.append(t)
+        index = bisect_left(self.ping_time, t-3000, self._min)
+        self._min = index
+        return len(self.ping_time) - index
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
