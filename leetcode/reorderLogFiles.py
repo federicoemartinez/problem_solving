ec773dc977a23483476f33298eb8cd3a4d34038d
Federico Martinez <federicoemartinez@gmail.com>
2018-11-12 22:02:11 -0300

    Solution for Reorder Log Files

---
+++ leetcode/reorderLogFiles.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/reorder-log-files/description/
+"""
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+ 
+
+Example 1:
+
+Input: ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+ 
+
+Note:
+
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] is guaranteed to have an identifier, and a word after the identifier.
+"""
+
+class Solution:
+    def reorderLogFiles(self, logs):
+        numeric_logs = []
+        alpha_logs = []
+        for each in logs:
+            values = each.split(" ")
+            if values[1].isnumeric():
+                numeric_logs.append(each)
+            else:
+                alpha_logs.append((values[1:], values[0], each))
+        alpha_logs.sort()
+        res = [x[2] for x in alpha_logs] + numeric_logs
+        return res
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        
